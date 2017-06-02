6487266b8ccef69e8735974d03e8bc1629aaf39d
Kristen Splonskowski <ksplons@atlassian.com>
2017-06-02 12:45:22 -0700

    fixed layout of GUI and cleaned up some stuff

--- GUI.py
+++ GUI.py
@@ -12,7 +12,7 @@ focal = 0.0
 prinX = 0.0
 prinY = 0.0
 base = 0.0
-enterFlag = False #To be used if entering from Samples
+enterFlag = False # to be used if entering from Samples
 
 # set up framework
 mainframe = ttk.Frame(root, padding="30 30 80 80")
@@ -22,6 +22,7 @@ mainframe.rowconfigure(0, weight=1)
 
 focalLength = StringVar(root)
 
+# extra spacing to make layout of GUI look nicer
 ttk.Label(mainframe, text="").grid(column=2, row=3, sticky=W)
 ttk.Label(mainframe, text="").grid(column=3, row=3, sticky=W)
 ttk.Label(mainframe, text="").grid(column=4, row=3, sticky=W)
@@ -48,19 +49,22 @@ baseline = StringVar(root)
 ttk.Label(mainframe, text="Baseline").grid(column=2, row=7)
 ttk.Entry(mainframe, text='', textvariable=baseline, width=8).grid(column=3, row=7)
 
+# extra spacing to make layout of GUI look nicer
 ttk.Label(mainframe, text="").grid(column=2, row=8, sticky=W)
 ttk.Label(mainframe, text="").grid(column=3, row=8, sticky=W)
 ttk.Label(mainframe, text="").grid(column=4, row=8, sticky=W)
 
+# function that is called when disparity button is pushed
 def disparity():
     subprocess.call(["./library", "disparity", leftFilePath, rightFilePath])
-    print('Disparity!')
-
+    
+    #print('Disparity!')
 
 # disparity button
 b = ttk.Button(mainframe, text="Disparity", command=disparity)
 b.grid(column=3, row=9, sticky=E)
 
+# function that is called when point cloud button is pushed
 def pointCloud():
     global enterFlag
     global focal
@@ -76,21 +80,25 @@ def pointCloud():
     temppath = leftFilePath[:-4] + '_disp.pgm'
 
     subprocess.call(["./library", "populate", leftFilePath, temppath, str(focal), str(base), str(prinX), str(prinY)])
-    print('Point Cloud!')
     enterFlag = False
+
+    #print('Point Cloud!')
 
 # point cloud button
 b2 = ttk.Button(mainframe, text="Point Cloud", command=pointCloud)
 b2.grid(column=4, row=9, sticky=S)
 
+# function that is called when visualize button is pushed
 def visualize(): #liz will fix this 
     subprocess.call(["./library", "visualize", "point_cloud.pcd"])
-    print('Visualize!')
+    
+    #print('Visualize!')
 
 # visualize button
 b3 = ttk.Button(mainframe, text="Visualize", command=visualize)
 b3.grid(column=5, row=9, sticky=W)
 
+# function to allow user to upload a left image
 def browseLeft():
     global leftFilePath
     leftFilePath = filedialog.askopenfilename()
@@ -99,6 +107,7 @@ def browseLeft():
 pathLeftLabel = Label(root)
 # pathLeftLabel.grid(column=3, row=2, sticky=W)
 
+# function to allow user to upload a right image
 def browseRight():
     global rightFilePath
     rightFilePath = filedialog.askopenfilename()
@@ -115,13 +124,15 @@ b4.grid(column=3, row=1)
 b5 = ttk.Button(mainframe, text="Upload Right Image", command=browseRight)
 b5.grid(column=5, row=1)
 
-ttk.Label(mainframe, text="").grid(column=1, row=10, sticky=W)
-ttk.Label(mainframe, text="").grid(column=2, row=10, sticky=W)
+# extra spacing to make layout of GUI look nicer
 ttk.Label(mainframe, text="").grid(column=3, row=10, sticky=W)
+ttk.Label(mainframe, text="").grid(column=4, row=10, sticky=W)
+ttk.Label(mainframe, text="").grid(column=5, row=10, sticky=W)
 ttk.Label(mainframe, text="").grid(column=1, row=11, sticky=W)
 ttk.Label(mainframe, text="").grid(column=2, row=11, sticky=W)
 ttk.Label(mainframe, text="").grid(column=3, row=11, sticky=W)
 
+# sample of two images converted to disparity image, point cloud and then fully visualized
 def sample1(): 
     global enterFlag
     enterFlag = True
@@ -146,8 +157,9 @@ def sample1():
 
 # sample 1 button
 b6 = ttk.Button(mainframe, text="Sample 1", command=sample1)
-b6.grid(column=3, row=10, sticky=E)
+b6.grid(column=3, row=11, sticky=E)
 
+# sample of two images converted to disparity image, point cloud and then fully visualized
 def sample2():
     global enterFlag
     enterFlag = True
@@ -172,8 +184,9 @@ def sample2():
 
 # sample 2 button
 b7 = ttk.Button(mainframe, text="Sample 2", command=sample2)
-b7.grid(column=4, row=10, sticky=E)
+b7.grid(column=4, row=11, sticky=S)
 
+# sample of two images converted to disparity image, point cloud and then fully visualized
 def sample3():
     global enterFlag
     enterFlag = True
@@ -189,7 +202,7 @@ def sample3():
     global leftFilePath 
     leftFilePath = 'samples/I1_000879.pgm'
     global rightFilePath 
-    rightFilePath = 'samples/I1_000879.pgm'
+    rightFilePath = 'samples/I2_000879.pgm'
 
     disparity()
     pointCloud()
@@ -198,25 +211,6 @@ def sample3():
 
 # sample 3 button
 b8 = ttk.Button(mainframe, text="Sample 3", command=sample3)
-b8.grid(column=3, row=11, sticky=E)
-
-# def sample4():
-
-#     focalLength = '1216.001'
-#     principleX = '672.99'
-#     principleY = '265.32'
-#     baseline = '357.8'
-
-#     leftFilePath = ''
-#     rightFilePath = ''
-
-#     disparity()
-#     pointCloud()
-#     visualize()
-#     print('Sample 4!')
-
-# # sample 4 button
-# b9 = ttk.Button(mainframe, text="Sample 4", command=sample4)
-# b9.grid(column=4, row=11, sticky=E)
+b8.grid(column=5, row=11, sticky=W)
 
 root.mainloop()
